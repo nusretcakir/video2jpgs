64d9a8aa9e914ac49e291503ef09d488bffa6822
nusretc <53660826+nusretc@users.noreply.github.com>
2022-04-15 15:46:22 +0300

    v1

---
+++ video2jpgs.py
@@ -0,0 +1,22 @@
+import cv2,os
+
+video_path=input("Video Yolu : ")
+vidcap = cv2.VideoCapture(video_path)               
+shutter = int(input("Kaç Framede bir resim alacaksiniz? :")) 
+
+success,image = vidcap.read()
+count = 0
+success = True
+
+if os.path.isdir("jpgs"):
+  pass
+else:
+  os.mkdir("jpgs")
+
+while success:
+  success,image = vidcap.read()
+  if count % shutter == 0:
+    cv2.imwrite("jpgs/frame%d.jpg" % count, image)     # save frame as JPEG file
+    if cv2.waitKey(10) == 27:                          # exit if Escape is hit
+          break
+  count += 1
